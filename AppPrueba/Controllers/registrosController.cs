ef0e0a85f7c23cd7e431d885781b29dbe35b688f
Santiago <snarinocardenas@gmail.com>
2021-06-27 21:40:38 -0500

    App web razor mvc

---
+++ AppPrueba/Controllers/registrosController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AppPrueba.Models;
+
+namespace AppPrueba.Controllers
+{
+    public class registrosController : Controller
+    {
+        private TecnoHardDBEntities db = new TecnoHardDBEntities();
+
+        // GET: registros
+        public ActionResult Index()
+        {
+            var registros = db.registros.Include(r => r.categorias).Include(r => r.productos).Include(r => r.proveedores);
+            return View(registros.ToList());
+        }
+
+        // GET: registros/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registros registros = db.registros.Find(id);
+            if (registros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registros);
+        }
+
+        // GET: registros/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdCate = new SelectList(db.categorias, "IdCate", "NombreCate");
+            ViewBag.IdProd = new SelectList(db.productos, "IdProd", "NombreProd");
+            ViewBag.IdProv = new SelectList(db.proveedores, "IdProv", "nombreProv");
+            return View();
+        }
+
+        // POST: registros/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdReg,IdProd,IdCate,IdProv")] registros registros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.registros.Add(registros);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdCate = new SelectList(db.categorias, "IdCate", "NombreCate", registros.IdCate);
+            ViewBag.IdProd = new SelectList(db.productos, "IdProd", "NombreProd", registros.IdProd);
+            ViewBag.IdProv = new SelectList(db.proveedores, "IdProv", "nombreProv", registros.IdProv);
+            return View(registros);
+        }
+
+        // GET: registros/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registros registros = db.registros.Find(id);
+            if (registros == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdCate = new SelectList(db.categorias, "IdCate", "NombreCate", registros.IdCate);
+            ViewBag.IdProd = new SelectList(db.productos, "IdProd", "NombreProd", registros.IdProd);
+            ViewBag.IdProv = new SelectList(db.proveedores, "IdProv", "nombreProv", registros.IdProv);
+            return View(registros);
+        }
+
+        // POST: registros/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdReg,IdProd,IdCate,IdProv")] registros registros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registros).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdCate = new SelectList(db.categorias, "IdCate", "NombreCate", registros.IdCate);
+            ViewBag.IdProd = new SelectList(db.productos, "IdProd", "NombreProd", registros.IdProd);
+            ViewBag.IdProv = new SelectList(db.proveedores, "IdProv", "nombreProv", registros.IdProv);
+            return View(registros);
+        }
+
+        // GET: registros/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registros registros = db.registros.Find(id);
+            if (registros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registros);
+        }
+
+        // POST: registros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            registros registros = db.registros.Find(id);
+            db.registros.Remove(registros);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Documentos/documentacion.md
@@ -0,0 +1,52 @@
+# Documentacion proyecto Technohard
+
+## Indice
+  - [Objetivo](#Objetivo)
+  - [Requerimientos](#Requeriientos)
+  - [Opciones de la pagina](#Opciones-de-la-pagina)
+    1. [Ingreso a la pagina](#Ingreso-a-la-pagina)
+    2. [Productos](#Productos)
+    3. [Contacto](#Contacto)
+ 
+ ## Objetivo
+
+Otorgar una guia de los procesos realizados para la creacion de la pagina web technoHard.
+
+## Requerimientos
+Los requerimientos son:
+
+- Conexion a internet estable.
+- Un dispositivo para conectarse (Computador, Tablet o Smartphone)
+- Un navegador web (Chrome, Explorer, Firefox, etc)
+
+## Opciones de la pagina
+
+La pagina esta divida en varias vistas para poder con interactuar el usuario.
+
+   ### Ingreso a la pagina
+
+   Cuando el usuario ingresa a la pagina, encontrará una vista de bienvenida con una breve descripcion de technohard, un carrousel de fotos donde mostrara algunos de los articulos, un menu superior que tendra botones que lo llevaran a las repectivas paginas como:
+
+ - Vision y Mision.
+ - Productos.
+ - Ofertas.
+ - Contacto.
+
+  Cada una de estas paginas tiene una descripcion de lo que ofrece la pagina.
+
+### Productos
+
+Al ingresar a esta pagina se mostrara un menu con las categorias de como estan organizados los productos como:
+
+- monitores
+- Audifonos
+- Teclados
+- mouses
+ 
+### Contacto
+
+En esta pagina encontrara una vista con las redes sociales de la empresa, e-mail, telefono y ubicacion por medio de la visualizacion de un mapa satelital.
+
+Tambien en esta vista encontrara un formulario para dejar una peticion, queja o recurso que quiera presentar el usuario a la compañia.
+
+
